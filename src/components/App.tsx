7a01cfc0b0368ffc8f3be9e73f256d670d54fddd
Jimmy <212524470+jimmy-codix@users.noreply.github.com>
2025-08-19 12:21:48 +0200

    moved around files

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcome to the Cafe Retro</h1>
+      <p>Enjoy our delicious coffee and pastries!</p>
+    </>
+  )
+}
+
+export default App
